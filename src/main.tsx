30048efacfa578b4bc8df0cca1ebd1fb10556101
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-07-02 10:11:08 +0700

    feat: add context config

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
+import AuthProvider from "./providers/auth-provider.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Toaster position="top-right" />
-        <App />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Toaster position="top-right" />
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );
