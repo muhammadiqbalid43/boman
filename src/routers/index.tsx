12b8eb397137fc54603c8b028c41caa176588620
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-07-02 10:10:40 +0700

    feat: add dashboard route

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,14 +1,27 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router";
 import { LoadingSpinner } from "../components/loading.tsx";
+import ProtectedRoute from "../features/auth/components/protected-route.tsx";
 
 const HomePage = lazy(() => import("../pages/home-page.tsx"));
+
+const SignInPage = lazy(() => import("../pages/sign-in-page.tsx"));
+const SignUpPage = lazy(() => import("../pages/sign-up-page.tsx"));
+
+const DashboardPage = lazy(() => import("../pages/dashboard-page.tsx"));
 
 const AppRoutes = () => {
   return (
     <Suspense fallback={<LoadingSpinner />}>
       <Routes>
         <Route path="/" element={<HomePage />} />
+
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
       </Routes>
     </Suspense>
   );
